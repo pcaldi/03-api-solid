3c6be7781282e05f7ae02011cd2d4321a09e8304
Paulo Ricardo Caldi <pcaldi@gmail.com>
2023-05-09 09:35:54 -0300

    chore: Use case pattern

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,7 +1,6 @@
 import { FastifyRequest, FastifyReply } from 'fastify';
-import { prisma } from '@/lib/prisma';
-import { hash } from 'bcryptjs';
 import { z } from 'zod';
+import { registerUseCase } from '@/use-cases/register';
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
@@ -12,23 +11,15 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
 
   const { name, email, password } = registerBodySchema.parse(request.body);
 
-  const password_hash = await hash(password, 6);
-
-  const userWithSameEmail = await prisma.user.findUnique({
-    where: {
+  try {
+    await registerUseCase({
+      name,
       email,
-    },
-  });
-  if (userWithSameEmail) {
+      password,
+    });
+  } catch (error) {
     return reply.status(409).send();
   }
 
-  await prisma.user.create({
-    data: {
-      name,
-      email,
-      password_hash,
-    },
-  });
   return reply.status(201).send();
 }
