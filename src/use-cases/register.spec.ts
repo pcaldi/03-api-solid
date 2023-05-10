e87434b7affa42c5acd557cc3e638f4d0c5f150e
Paulo Ricardo Caldi <pcaldi@gmail.com>
2023-05-10 18:08:04 -0300

    chore: Test and Authenticate controllers

--- src/use-cases/register.spec.ts
+++ src/use-cases/register.spec.ts
@@ -4,7 +4,7 @@ import { compare } from 'bcryptjs';
 import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository';
 import { UserAlreadyExistsError } from './errors/user-already-exists-error';
 
-describe('Register use case', () => {
+describe('Register Use Case', () => {
   it('should be able to register', async () => {
     const usersRepository = new InMemoryUsersRepository();
     const registerUseCase = new RegisterUseCase(usersRepository);
